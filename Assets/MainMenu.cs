eb29927c6c9d39701055e8177bfc0631762a1f22
raushan2ashish <raushan22@live.com>
2024-12-11 23:53:30 -0500

    linked quit button

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -12,7 +12,13 @@ public class MainMenu : MonoBehaviour
     }
     public void QuitGame()
     {
+        // For the editor, log a message since Application.Quit does not work in the editor.
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stop play mode in the Unity editor
+        #else
+        Application.Quit(); // Quit the application in a built game
+        #endif
 
-        Application.Quit();
+
     }
 }
