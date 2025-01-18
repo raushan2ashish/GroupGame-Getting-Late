c4d0195742715be1ac436b8210d2a762aa403697
Fawkewe <142065200+Fawkewe@users.noreply.github.com>
2025-01-18 13:16:18 -0500

    Timer implemented

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
     public float timer = 0.0f;
+    public Text text;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //text.text = "Time: " + timer;
     }
 
-    // Update is called once per frame
+    // Update is called once per frameW 
     void Update()
     {
         timer += Time.deltaTime;
+        text.text = "Time: " + timer;
     }
 
 }
