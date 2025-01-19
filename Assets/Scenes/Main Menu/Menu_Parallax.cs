8d0dcba683d805336585425c930018e04aff7607
raushan2ashish <raushan22@live.com>
2025-01-18 19:07:04 -0500

    Redesigned the Main Menu

---
+++ Assets/Scenes/Main Menu/Menu_Parallax.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Parallax : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = 0.3f;
+    private Vector2 startPosition;
+    private Vector3 velocity;
+    void Start()
+    {
+        startPosition = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+    }
+}
